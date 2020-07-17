3ba559bd66b02a2f998256ee5114061d7b72b699
陳劭愷 <mail@justin0u0.com>
2020-07-17 08:58:47 +0800

    accept/pow(x,n)

---
+++ july-leetcode-challenge/pow-x-n/solution.cpp
@@ -0,0 +1,12 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/explore/featured/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3392/
+ */
+
+class Solution {
+public:
+  double myPow(double x, int n) {
+    return pow(x, n);
+  }
+};
+
