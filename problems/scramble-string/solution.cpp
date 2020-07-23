c182c3c4e4178313e1cb7649ee472faaf4aea2a1
陳劭愷 <mail@justin0u0.com>
2020-07-24 02:38:37 +0800

    accept/scramble-string

---
+++ problems/scramble-string/solution.cpp
@@ -0,0 +1,10 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/scramble-string/
+ */
+
+class Solution {
+public:
+  bool isScramble(string s1, string s2) {
+  }
+};
