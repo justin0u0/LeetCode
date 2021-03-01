9c1dae72e5d8b14149663f7ce5616b5df57d2df9
陳劭愷 <mail@justin0u0.com>
2021-03-01 20:37:44 +0800

    Finish LeetCode 141 - linked list cycle

---
+++ problems/0141-linked-list-cycle/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/linked-list-cycle/
+ * Runtime: 8ms
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+    if (head == nullptr)
+      return false;
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
