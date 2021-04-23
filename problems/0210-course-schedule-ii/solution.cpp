bd9a9c9851f0603425487f54be1566e5d1d6320a
陳劭愷 <mail@justin0u0.com>
2021-04-24 01:11:19 +0800

    Finish LeetCode 210 - Course Schedule II

---
+++ problems/0210-course-schedule-ii/solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/course-schedule-ii/
+ * Runtime: 20ms
+ */
+
+class Solution {
+private:
+  vector<vector<int>> G;
+  vector<bool> vis;
+  vector<bool> instack;
+  vector<int> courses;
+  bool topological_sort(int u) {
+    vis[u] = true;
+    instack[u] = true;
+    for (auto v: G[u]) {
+      if (instack[v]) return false;
+      else if (!vis[v] && !topological_sort(v)) return false;
+    }
+    instack[u] = false;
+    courses.emplace_back(u);
+    return true;
+  }
+public:
+  vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    G.resize(numCourses);
+    vis.resize(numCourses, false);
+    instack.resize(numCourses, false);
+    for (auto prerequisite: prerequisites) {
+      G[prerequisite[0]].emplace_back(prerequisite[1]);
+    }
+
+    for (int i = 0; i < numCourses; i++) {
+      if (!vis[i] && !topological_sort(i)) {
+        courses.clear();
+        return courses;
+      }
+    }
+    return courses;
+  }
+};
+
