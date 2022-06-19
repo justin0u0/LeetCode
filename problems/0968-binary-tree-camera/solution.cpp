e65e8a48847ecc2d5773fc3e0e9d298c3ea1121c
justin0u0 <38185674+justin0u0@users.noreply.github.com>
2022-06-19 13:20:33 +0800

    Update LeetCode 968 - Binary Tree Camera

--- problems/0968-binary-tree-camera/solution.cpp
+++ problems/0968-binary-tree-camera/solution.cpp
@@ -12,11 +12,11 @@
  * 	dp(u, 2) -> no camera, NOT covered by child
  * 		= dp(l, 1) + dp(r, 1)
  *
- * Edge cases (leaf):
- * 	dp(leaf, 0) = 1, dp(leaf, 1) = inf, dp(leaf, 2) = 0
+ *  Edge cases (leaf):
+ * 	  dp(leaf, 0) = 1, dp(leaf, 1) = inf, dp(leaf, 2) = 0
  *
- * Answer:
- * 	min(dp(root, 0), dp(root, 1))
+ *  Answer:
+ * 	  min(dp(root, 0), dp(root, 1))
  */
 
 #define min3(a, b, c) min(a, min(b, c))
@@ -41,7 +41,6 @@ private:
     int dp1 = min3(dp(l, 0) + dp(r, 1), dp(l, 1) + dp(r, 0), dp(l, 0) + dp(r, 0));
     int dp2 = dp(l, 1) + dp(r, 1);
 
-    // cout << min(dp0, mask) << ' ' << min(dp1, mask) << ' ' << min(dp2, mask) << endl;
     return (min(dp2, mask)<<20) + (min(dp1, mask)<<10) + min(dp0, mask);
   }
 public:
