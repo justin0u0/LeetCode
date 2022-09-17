c0c9cbe470fd3768728b6d64bcb1f6e301be8b11
justin0u0 <mail@justin0u0.com>
2022-09-18 02:52:04 +0800

    Finish LeetCode 1770 - Maximum Score from Performing Multiplication Operations

---
+++ 1000-1999/1770-maximum-score-from-performing-multiplication-operations/solution2.cpp
@@ -0,0 +1,46 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/maximum-score-from-performing-multiplication-operations/
+ * Runtime: 345ms (97.03%)
+ * Time Complexity: O(M^2)
+ * Space Complexity: O(M)
+ * Description:
+ *  dp(i, j) is the maximum score when using the first i multipliers with j multipliers apply from
+ *  the left.
+ *
+ *  dp(i, j) = max{
+ *    dp(i - 1, j - 1) + multipliers[i] * nums[take from left],
+ *    dp(i - 1, j) + multipliers[i] * nums[take from right]
+ *  }
+ */
+
+class Solution {
+public:
+  int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+    int n = nums.size();
+    int m = multipliers.size();
+    vector<vector<int>> dp(2, vector<int>(m + 1, -0x3f3f3f3f));
+
+    dp[0][0] = 0;
+    for (int i = 1; i <= m; ++i) {
+      vector<int>& cur = dp[i & 1];
+      vector<int>& pre = dp[!(i & 1)];
+
+      cur[0] = pre[0] + multipliers[i - 1] * nums[n - i];
+      cur[i] = pre[i - 1] + multipliers[i - 1] * nums[i - 1];
+      for (int j = 1; j < i; ++j) {
+        cur[j] = max(
+          pre[j - 1] + multipliers[i - 1] * nums[j - 1],
+          pre[j] + multipliers[i - 1] * nums[n - (i - j)]
+        );
+      }
+    }
+
+    int answer = -0x3f3f3f3f;
+    vector<int>& result = dp[m & 1];
+    for (int i = 0; i <= m; ++i) {
+      answer = max(answer, result[i]);
+    }
+    return answer;
+  }
+};
