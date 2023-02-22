7fb06785090f61e7e47fb833bef32e09595b1a1e
justin0u0 <mail@justin0u0.com>
2023-02-22 14:24:35 +0800

    Finish LeetCode 1011 - Capacity To Ship Packages Within D Days

---
+++ problems/1011-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+ * Runtime: 42ms (95.21%)
+ */
+
+class Solution {
+private:
+  inline bool check(const vector<int>& weights, int cap, int days) {
+    int d = 1;
+    int sum = 0;
+    for (const int& w : weights) {
+      if (w > cap) {
+        return false;
+      }
+      if (sum + w > cap) {
+        if (d == days) {
+          return false;
+        }
+        ++d;
+        sum = w;
+      } else {
+        sum += w;
+      }
+    }
+
+    return true;
+  }
+public:
+  int shipWithinDays(const vector<int>& weights, int days) {
+    int l = 0;
+    int r = accumulate(weights.begin(), weights.end(), 0);
+    while (l <= r) {
+      int mid = (l + r) >> 1;
+      if (check(weights, mid, days)) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return r + 1;
+  }
+};
