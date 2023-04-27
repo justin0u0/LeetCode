927daa1815272dcfbef84d7dfda689a5a1ca64aa
justin0u0 <mail@justin0u0.com>
2023-04-27 16:44:24 +0800

    Finish LeetCode 319 - Bulb Switcher

---
+++ problems/0319-bulb-switcher/solution.cpp
@@ -0,0 +1,12 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/bulb-switcher/
+ * Runtime: 0ms (100.00%)
+ */
+
+class Solution {
+public:
+  int bulbSwitch(int n) {
+    return (int)sqrt(n);
+  }
+};
