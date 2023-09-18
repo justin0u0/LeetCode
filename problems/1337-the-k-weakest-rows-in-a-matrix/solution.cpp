16fe05818980dc652b9e7f72aea6427ac8931d91
justin0u0 <mail@justin0u0.com>
2023-09-18 22:49:02 +0800

    Finish LeetCode 1337 - The K Weakest Rows in a Matrix

--- problems/1337-the-k-weakest-rows-in-a-matrix/solution.cpp
+++ problems/1337-the-k-weakest-rows-in-a-matrix/solution.cpp
@@ -28,3 +28,28 @@ public:
     return answer;
   }
 };
+
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+ * Runtime: 7ms (82.57%)
+ */
+
+class Solution {
+public:
+  vector<int> kWeakestRows(const vector<vector<int>>& mat, int k) {
+    vector<pair<int, int>> box(mat.size());
+    for (int i = 0; i < mat.size(); ++i) {
+      const auto& row = mat[i];
+      box[i] = {count(row.begin(), row.end(), 1), i};
+    }
+    sort(box.begin(), box.end());
+
+    vector<int> answer;
+    answer.reserve(mat.size());
+    transform(box.begin(), box.begin() + k, back_inserter(answer), [](const pair<int, int>& p) {
+      return p.second;
+    });
+    return answer;
+  }
+};
