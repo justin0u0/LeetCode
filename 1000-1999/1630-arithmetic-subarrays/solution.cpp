f41ff02e4d757255b0ea796f726a7da4b1fc767f
justin0u0 <mail@justin0u0.com>
2023-11-24 00:32:08 +0800

    Finish LeetCode 1630 - Arithmetic Subarrays

---
+++ 1000-1999/1630-arithmetic-subarrays/solution.cpp
@@ -0,0 +1,7 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/arithmetic-subarrays/
+ * Runtime: 28ms (94.97%)
+ */
+
+https://leetcode.com/problems/arithmetic-subarrays/
