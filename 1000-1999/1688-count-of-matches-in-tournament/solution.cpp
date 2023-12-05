131fb6bb206a0a92a87ccfa1365d0e0ab891646f
justin0u0 <mail@justin0u0.com>
2023-12-05 20:50:40 +0800

    Finish LeetCode 1688 - Count of Matches in Tournament

---
+++ 1000-1999/1688-count-of-matches-in-tournament/solution.cpp
@@ -0,0 +1,12 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/count-of-matches-in-tournament/
+ * Runtime: 0ms (100.00%)
+ */
+
+class Solution {
+public:
+  int numberOfMatches(int n) {
+    return n - 1;
+  }
+};
