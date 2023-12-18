90b91a43204aca3aef995b2a6ab469ad3be607fd
justin0u0 <mail@justin0u0.com>
2023-12-18 21:33:55 +0800

    Finish LeetCode 1913 - Maximum Product Difference Between Two Pairs

---
+++ 1000-1999/1913-maximum-product-difference-between-two-pairs/solution.cpp
@@ -0,0 +1,19 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+ * Runtime: 14ms (96.75%)
+ */
+
+const static auto _ = []() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  return nullptr;
+}();
+
+class Solution {
+public:
+  int maxProductDifference(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    const int n = nums.size();
+    return nums[n - 1] * nums[n - 2] - nums[1] * nums[0];
+  }
+};
