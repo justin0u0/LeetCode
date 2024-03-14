18a661a16a197eab415f6d50e82de8c3693cb273
justin0u0 <mail@justin0u0.com>
2024-03-14 21:26:03 +0800

    Finish LeetCode 930 - Binary Subarrays With Sum

---
+++ 0000-0999/0930-binary-subarrays-with-sum/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/binary-subarrays-with-sum/
+ * Runtime: 33ms (59.47%)
+ */
+
+const static auto _ = []() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+  return nullptr;
+}();
+
+class Solution {
+public:
+  int numSubarraysWithSum(const vector<int>& nums, int goal) {
+    unordered_map<int, int> m;
+    int sum = 0;
+    m.emplace(0, 1);
+    int answer = 0;
+    for (int num : nums) {
+      sum += num;
+      answer += m[sum - goal];
+      ++m[sum];
+    }
+    return answer;
+  }
+};
