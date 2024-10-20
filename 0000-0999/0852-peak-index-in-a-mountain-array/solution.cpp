fc59edc6a3c3684593dd117e7e0494ebbce9b1e1
justin0u0 <mail@justin0u0.com>
2024-10-20 21:09:10 +0800

    Finish LeetCode 852 - Peak Index in a Mountain Array

--- 0000-0999/0852-peak-index-in-a-mountain-array/solution.cpp
+++ 0000-0999/0852-peak-index-in-a-mountain-array/solution.cpp
@@ -29,3 +29,26 @@ public:
     return -1;
   }
 };
+
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/peak-index-in-a-mountain-array/
+ * Runtime: 0ms (100.00%)
+ */
+
+class Solution {
+public:
+  int peakIndexInMountainArray(const vector<int>& arr) {
+    int l = 1;
+    int r = arr.size() - 1;
+    while (l < r) {
+      int mid = (l + r) >> 1;
+      if (arr[mid] > arr[mid + 1]) {
+        r = mid;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return r;
+  }
+};
