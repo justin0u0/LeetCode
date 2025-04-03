50dd5781700d82e82e7b134e1550f9d641627360
justin0u0 <mail@justin0u0.com>
2025-04-03 19:02:25 +0800

    Finish LeetCode 2873 - Maximum Value of an Ordered Triplet I

---
+++ problems/2873-maximum-value-of-an-ordered-triplet-i/solution.cpp
@@ -0,0 +1,20 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/
+ * Runtime: 0ms (100.00%)
+ */
+
+class Solution {
+public:
+  long long maximumTripletValue(vector<int>& nums) {
+    int mx = 0;
+    int mxd = 0;
+    long long res = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      res = max(res, 1LL * mxd * nums[i]);
+      mxd = max(mxd, mx - nums[i]);
+      mx = max(mx, nums[i]);
+    }
+    return res;
+  }
+};
