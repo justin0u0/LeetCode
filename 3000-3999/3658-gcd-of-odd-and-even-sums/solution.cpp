a070992182b1acfd71ded422c5c35886708f8408
justin0u0 <mail@justin0u0.com>
2026-07-15 21:54:25 +0800

    Finish LeetCode 3658 - GCD of Odd and Even Sums

---
+++ 3000-3999/3658-gcd-of-odd-and-even-sums/solution.cpp
@@ -0,0 +1,12 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+ * Runtime: 0ms (100.00%)
+ */
+
+class Solution {
+public:
+  int gcdOfOddEvenSums(const int n) {
+    return n;
+  }
+};
