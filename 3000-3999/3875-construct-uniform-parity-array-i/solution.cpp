ba256ff3f81e80a77a5273c0ec7481c1310cd5ca
justin0u0 <mail@justin0u0.com>
2026-09-03 02:24:51 +0800

    Finish LeetCode 3875 - Construct Uniform Parity Array I

---
+++ 3000-3999/3875-construct-uniform-parity-array-i/solution.cpp
@@ -0,0 +1,15 @@
+/**
+ * Author: justin0u0<mail@justin0u0.com>
+ * Problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ * Runtime: 0ms (100.00%)
+ */
+
+class Solution {
+public:
+  bool uniformArray(vector<int>& nums1) {
+    // odd > 0   -> all odds
+    // otherwise -> all evens
+
+    return true;
+  }
+};
